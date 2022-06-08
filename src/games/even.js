b1ea6a9ec20c51eee5235f7dc0340ab580722d85
Anna Petrova <petrovanna91@mail.ru>
2022-06-08 20:22:01 +0300

    Remove const greeting

--- src/games/even.js
+++ src/games/even.js
@@ -3,8 +3,7 @@ import readlineSync from 'readline-sync';
 const playBrainEven = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  const greeting = `Hello, ${userName}!`;
-  console.log(greeting);
+  console.log(`Hello, ${userName}!`);
 
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
