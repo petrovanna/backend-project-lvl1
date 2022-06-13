6367175e1544a91e7cb0862ee2d7512dc4cf06a7
Anna Petrova <petrovanna91@mail.ru>
2022-06-13 23:04:48 +0300

    Add functions: getGameDiscription, getAQuastion, finishGame1/2, congratulateUser

--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
 
-export const playBrainGames = (discriptionOfGame) => {
-  console.log(discriptionOfGame);
+export const getGameDiscription = (printDiscriptionOfGame) => {
+  console.log(printDiscriptionOfGame);
 };
 
-export const question = (gameQuestion) => {
-  console.log(`Question: ${gameQuestion}`);
+export const getAQuestion = (playBrainGame) => {
+  console.log(`Question: ${playBrainGame}`);
+};
+
+export const finishGame1 = (answer) => {
+  const userLost1 = `"${answer}" is wrong answer ;(. Correct answer was "yes".\nLet's try again, ${userName}!`;
+  console.log(userLost1);
+};
+
+export const finishGame2 = (answer) => {
+  const userLost2 = `"${answer}" is wrong answer ;(. Correct answer was "no".\nLet's try again, ${userName}!`;
+  console.log(userLost2);
+};
+
+export const congratulateUser = () => {
+  const congratulations = `Congratulations, ${userName}!`;
+  console.log(congratulations);
 };
