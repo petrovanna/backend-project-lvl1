d1cf0b3406a899b8301ccd41ea7c0c5f4c007950
Anna Petrova <petrovanna91@mail.ru>
2022-06-16 17:47:40 +0300

    Remove extra code, use index.js code

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
-/* import playBrainEven from '../src/games/even.js';
-
-console.log(playBrainEven()); */
-
 import { printDiscriptionOfGame, playBrainGame } from '../src/games/even.js';
 
 printDiscriptionOfGame();
