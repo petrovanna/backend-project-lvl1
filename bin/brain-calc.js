8107a4d9bebecb1bcdbd80f0e2553a2039a7dd86
Anna Petrova <petrovanna91@mail.ru>
2022-06-17 18:16:30 +0300

    Add printDiscriptionOfGame(); playBrainGame();

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
 
-/* import playBrainCalc from '../src/games/calc.js';
-
-console.log(playBrainCalc()); */
-
 import { printDiscriptionOfGame, playBrainGame } from '../src/games/calc.js';
 
 printDiscriptionOfGame();
