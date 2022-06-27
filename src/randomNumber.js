d14f206ce4fca9f79be2a58a938b1ba21e1399ab
Anna Petrova <petrovanna91@mail.ru>
2022-06-27 17:15:19 +0300

    Get a random integer between two values

--- src/randomNumber.js
+++ src/randomNumber.js
@@ -1,5 +1,6 @@
-const getRandomNumber = (num) => {
-  const randomNumber = Math.floor(Math.random() * num);
-  return randomNumber;
+const getRandomNumber = (min, max) => {
+  const minNum = Math.ceil(min);
+  const maxNum = Math.floor(max);
+  return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 };
 export default getRandomNumber;
