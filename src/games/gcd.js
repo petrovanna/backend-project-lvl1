22b2c8553040d656b88e8d0973b28de60e5d2e3f
Anna Petrova <petrovanna91@mail.ru>
2022-06-28 17:41:37 +0300

    Remove extra code inside getGreatestCommonDivisor()

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,7 +3,7 @@ import getRandomNumber from '../randomNumber.js';
 
 const discriptionOfGame = 'Find the greatest common divisor of given numbers.';
 
-const getGreatestCommonDivisor = (num1, num2) => {
+/* const getGreatestCommonDivisor = (num1, num2) => {
   if (num1 === 0) {
     return num2;
   } if (num2 === 0) {
@@ -17,6 +17,13 @@ const getGreatestCommonDivisor = (num1, num2) => {
     return iter(counter, acc % counter);
   };
   return iter(num2, remainder);
+}; */
+
+const getGreatestCommonDivisor = (acc, counter) => {
+  if (counter === 0) {
+    return acc;
+  }
+  return getGreatestCommonDivisor(counter, acc % counter);
 };
 
 const getQuestionAndRightAnswer = () => {
