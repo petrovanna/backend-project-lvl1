3673335fbd9eaa40fa133f20f29931b1e71a2ee2
Anna Petrova <petrovanna91@mail.ru>
2022-06-28 18:22:07 +0300

    Change getResultOfTheExpression. Now function use 3 parametrs instead of string

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,7 +3,7 @@ import getRandomNumber from '../randomNumber.js';
 
 const discriptionOfGame = 'What is the result of the expression?';
 
-const getResultOfTheExpression = (str) => {
+/* const getResultOfTheExpression = (str) => {
   const arr = str.split([' ']);
   switch (arr[1]) {
     case '+':
@@ -13,15 +13,26 @@ const getResultOfTheExpression = (str) => {
     default:
   }
   return Number(arr[0]) * Number(arr[2]);
+}; */
+
+const getResultOfTheExpression = (number1, number2, operation) => {
+  switch (operation) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    default:
+  }
+  return number1 * number2;
 };
 
 const getQuestionAndRightAnswer = () => {
   const number1 = getRandomNumber(0, 100);
   const number2 = getRandomNumber(0, 10);
-  const operators = ['+', '-', '*'];
-  const randomOperator = operators[getRandomNumber(0, 2)];
-  const randomExpression = `${number1} ${randomOperator} ${number2}`;
-  const rightAnswer = String(getResultOfTheExpression(randomExpression));
+  const operations = ['+', '-', '*'];
+  const randomOperation = operations[getRandomNumber(0, 2)];
+  const randomExpression = `${number1} ${randomOperation} ${number2}`;
+  const rightAnswer = String(getResultOfTheExpression(number1, number2, randomOperation));
   return [randomExpression, rightAnswer];
 };
 
