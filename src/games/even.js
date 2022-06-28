a8632d1ff9e32fd4c59fac244f40da9b9ad3471b
Anna Petrova <petrovanna91@mail.ru>
2022-06-28 13:56:49 +0300

    Remove is EvenNumber, addisEven() and check()

--- src/games/even.js
+++ src/games/even.js
@@ -3,11 +3,17 @@ import getRandomNumber from '../randomNumber.js';
 
 const discriptionOfGame = 'Answer \'yes\' if the number is even, otherwise answer \'no\'.';
 
-const isNumberEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
+// const isNumberEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
+
+const isEven = (number) => (number % 2 === 0);
+const check = (number) => {
+  const result = isEven(number) ? 'yes' : 'no';
+  return result;
+};
 
 const getQuestionAndRightAnswer = () => {
   const number = getRandomNumber(1, 100);
-  const rightAnswer = isNumberEven(number);
+  const rightAnswer = check(number);
   return [number, rightAnswer];
 };
 
