ff6e1e6197d3a331722ef1cd2a365c087bd8a84b
Anna Petrova <petrovanna91@mail.ru>
2022-06-28 18:03:02 +0300

    Fix case Question: 1

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,7 +4,7 @@ import getRandomNumber from '../randomNumber.js';
 const discriptionOfGame = 'Answer \'yes\' if given number is prime. Otherwise answer \'no\'.';
 
 const isPrime = (number) => {
-  if (number === 0) {
+  if (number === 0 || number === 1) {
     return true;
   } for (let i = 2; i <= number / 2; i += 1) {
     const remainder = number % i === 0;
