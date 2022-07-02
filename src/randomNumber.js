ee938dbc34ea69f25a5d5948f39fe130aeb6ee92
Anna Petrova <petrovanna91@mail.ru>
2022-07-02 22:05:25 +0300

    Remove number rounding

--- src/randomNumber.js
+++ src/randomNumber.js
@@ -1,6 +1,2 @@
-const getRandomNumber = (min, max) => {
-  const minNum = Math.ceil(min);
-  const maxNum = Math.floor(max);
-  return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-};
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 export default getRandomNumber;
