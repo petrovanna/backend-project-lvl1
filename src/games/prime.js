fe3dcb51b1ad5b3936bb555bc1b8b2a711d2af4c
Anna Petrova <petrovanna91@mail.ru>
2022-07-14 00:48:00 +0300

    Fix wrong logic in isPrime()

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,20 +4,20 @@ import getRandomNumber from '../randomNumber.js';
 const discriptionOfGame = 'Answer \'yes\' if given number is prime. Otherwise answer \'no\'.';
 
 const isPrime = (number) => {
-  if (number === 0 || number === 1) {
-    return true;
+  if (number < 2) {
+    return false;
   } for (let i = 2; i <= number / 2; i += 1) {
     const remainder = number % i === 0;
     if (remainder) {
-      return true;
+      return false;
     }
   }
-  return false;
+  return true;
 };
 
 const getQuestionAndRightAnswer = () => {
   const number = getRandomNumber(0, 100);
-  const rightAnswer = isPrime(number) ? 'no' : 'yes';
+  const rightAnswer = isPrime(number) ? 'yes' : 'no';
   return [number, rightAnswer];
 };
 
